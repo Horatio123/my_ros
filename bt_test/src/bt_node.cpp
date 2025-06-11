f0a33552d0d0a40f0fe36afe2e77038b32f2c600
Horatio <alexanderchen1992@gmail.com>
2025-06-11 22:33:42 +0800

    modify behaviortree_cpp_v3

--- bt_test/src/bt_node.cpp
+++ bt_test/src/bt_node.cpp
@@ -2,13 +2,32 @@
 
 using namespace BT;
 
+// Simple tree, used to execute once each action.
+static const char *xml_text = R"(
+
+<root >
+    <BehaviorTree>
+       <Sequence>
+           <CalculateNum   goal="{GoalPosition}" />
+           <PrintNum     target="{GoalPosition}" />
+           <Action_A/>
+           <Action_A_P/>
+       </Sequence>
+    </BehaviorTree>
+</root>
+)";
 
 class CalculateNum : public SyncActionNode
 {
 public:
-  CalculateNum(const std::string& name, const NodeConfiguration& config) :
-    SyncActionNode(name, config)
-  {}
+  CalculateNum(const std::string &name, const NodeConfiguration &config) : SyncActionNode(name, config)
+  {
+  }
+
+  static PortsList providedPorts()
+  {
+    return {OutputPort<int>("goal")};
+  }
 
   NodeStatus tick() override
   {
@@ -16,18 +35,21 @@ public:
     setOutput("goal", mygoal);
     return NodeStatus::SUCCESS;
   }
-  static PortsList providedPorts()
-  {
-    return {OutputPort<int>("goal")};
-  }
 };
 
 class PrintNum : public SyncActionNode
 {
 public:
-  PrintNum(const std::string& name, const NodeConfiguration& config) :
-    SyncActionNode(name, config)
-  {}
+  PrintNum(const std::string &name, const NodeConfiguration &config) : SyncActionNode(name, config)
+  {
+  }
+
+  static PortsList providedPorts()
+  {
+    // Optionally, a port can have a human readable description
+    const char *description = "Simply print the target on console...";
+    return {InputPort<int>("target", description)};
+  }
 
   NodeStatus tick() override
   {
@@ -40,34 +62,26 @@ public:
     printf("Num: [ %d]\n", goal);
     return NodeStatus::SUCCESS;
   }
-
-  static PortsList providedPorts()
-  {
-    // Optionally, a port can have a human readable description
-    const char* description = "Simply print the target on console...";
-    return {InputPort<int>("target", description)};
-  }
 };
-
 
 // Action_A has a different constructor than the default one.
 class Action_A : public SyncActionNode
 {
 public:
   // additional arguments passed to the constructor
-  Action_A(const std::string& name, const NodeConfiguration& config, int arg1,
-           double arg2, std::string arg3) :
-    SyncActionNode(name, config), _arg1(arg1), _arg2(arg2), _arg3(arg3)
-  {}
-
-  NodeStatus tick() override
+  Action_A(const std::string &name, const NodeConfiguration &config, int arg1,
+           double arg2, std::string arg3) : SyncActionNode(name, config), _arg1(arg1), _arg2(arg2), _arg3(arg3)
   {
-    std::cout<< "Action_A: " << _arg1 << " / " << _arg2 << " / " << _arg3 << std::endl;
-    return NodeStatus::SUCCESS;
   }
   static PortsList providedPorts()
   {
     return {};
+  }
+
+  NodeStatus tick() override
+  {
+    std::cout << "Action_A: " << _arg1 << " / " << _arg2 << " / " << _arg3 << std::endl;
+    return NodeStatus::SUCCESS;
   }
 
 private:
@@ -77,29 +91,17 @@ private:
 };
 
 
-// Simple tree, used to execute once each action.
-static const char* xml_text = R"(
-
- <root >
-     <BehaviorTree>
-        <Sequence>
-            <CalculateNum   goal="{GoalPosition}" />
-            <PrintNum     target="{GoalPosition}" />
-            <Action_A/>
-            <Action_A_P/>
-        </Sequence>
-     </BehaviorTree>
- </root>
- )";
-
 int main()
 {
   BehaviorTreeFactory factory;
+  factory.registerNodeType<CalculateNum>("CalculateNum");
+  factory.registerNodeType<PrintNum>("PrintNum");
 
   // A node builder is nothing more than a function pointer to create a
   // std::unique_ptr<TreeNode>.
   // Using lambdas or std::bind, we can easily "inject" additional arguments.
-  NodeBuilder builder_A = [](const std::string& name, const NodeConfiguration& config) {
+  NodeBuilder builder_A = [](const std::string &name, const NodeConfiguration &config)
+  {
     return std::make_unique<Action_A>(name, config, 42, 3.14, "hello world");
   };
 
@@ -107,20 +109,16 @@ int main()
   // Not the most user friendly, but definitely the most flexible one.
   factory.registerBuilder<Action_A>("Action_A", builder_A);
 
-  NodeBuilder builder_A_plus = [](const std::string& name, const NodeConfiguration& config) {
+  NodeBuilder builder_A_plus = [](const std::string &name, const NodeConfiguration &config)
+  {
     return std::make_unique<Action_A>(name, config, 55, 7.77, "hello earth");
   };
-
-  factory.registerNodeType<CalculateNum>("CalculateNum");
-  factory.registerNodeType<PrintNum>("PrintNum");
-
 
   // BehaviorTreeFactory::registerBuilder is the more general way to register a custom node.
   // Not the most user friendly, but definitely the most flexible one.
   factory.registerBuilder<Action_A>("Action_A_P", builder_A_plus);
 
   auto tree = factory.createTreeFromText(xml_text);
-
 
   // tree.tickRootWhileRunning();
   tree.tickRoot();
